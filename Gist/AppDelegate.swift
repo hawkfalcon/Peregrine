4356e2a746021de58ae83ce3e691ae0a0b69f9fd
hawkfalcon <contact@hawkfalcon.com>
2018-01-09 21:50:04 -0800

    Initial Commit

---
+++ Gist/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gist
+//
+//  Created by Tristen Miller on 1/9/18.
+//  Copyright © 2018 Tristen Miller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
